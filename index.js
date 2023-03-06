1ba1fd655370db9c66c573a724ea7ee84c8f5145
Devansh Chaudhary <58871818+devansh016@users.noreply.github.com>
2023-03-06 14:18:48 +0530

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const database = require("./utils/database");
+var cors = require('cors')
 const port = process.env.PORT || 80;
 
 // const winston = require('winston');
@@ -11,6 +12,7 @@ const port = process.env.PORT || 80;
 // };
 // const logger = winston.createLogger(logConfiguration);
 
+app.use(cors())
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: false }));
 
